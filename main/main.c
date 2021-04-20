23804c1a0ae33addc4510a23f56bf75d125998fc
Tomas Ferraro <tomas.ferraro@mercadolibre.com>
2021-04-20 00:24:02 +0000

    Added a tryout repo example

---
+++ main/main.c
@@ -0,0 +1,23 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void *thread(void *params) {
+    printf("Thread - %ld\n", pthread_self());
+    sleep(60);
+    printf("Finish Thread - %ld\n", pthread_self());
+}
+
+int main(int argc, char **argv) {
+    printf("Starting...\n");
+    // create the thread objs
+    pthread_t thread1, thread2;
+    // start the threads
+    pthread_create(&thread1, NULL, *thread, NULL);
+    pthread_create(&thread2, NULL, *thread, NULL);
+    // wait for threads to finish
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    return 0;
+}
